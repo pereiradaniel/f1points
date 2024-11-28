613eb1301e9398606cc618adfcc255ee3f123809
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2024-11-28 15:39:52 -0500

    Updated f1points.cpp to use private and public functions.

--- f1points/f1points.cpp
+++ f1points/f1points.cpp
@@ -30,107 +30,116 @@ const struct Points {
 
 // Struct for a driver's total points over a season:
 struct F1DriverPoints {
-    int total_points;   // Driver's total points
-    
-    // Calculates the number of points awarded for fastest laps:
-    void fastest_lap (int multiplier) {
-        for (int i = 1; i <= multiplier; ++i)
-            total_points += points.fastest_lap;
-    };
+    private:
+        int total_points = 0;   // Driver's total points;
 
-    // Calculates how many points earned for finishing position in a Sprint Race:
-    void sprint_race_points(int position) {
-        if (position == 1)
-            total_points += points.first_sprint;
-        if (position == 2)
-            total_points += points.second_sprint;
-        if (position == 3)
-            total_points += points.third_sprint;
-        if (position == 4)
-            total_points += points.fourth_sprint;
-        if (position == 5)
-            total_points += points.fifth_sprint;
-        if (position == 6)
-            total_points += points.sixth_sprint;
-        if (position == 7)
-            total_points += points.seventh_sprint;
-        if (position == 8)
-            total_points += points.eighth_sprint; 
-    }
+    public:
+        // Calculates the number of points awarded for fastest laps:
+        // Takes parameter multiplier to represent number of times fastest lap has been awarded.
+        void fastest_lap (int multiplier) {
+            for (int i = 1; i <= multiplier; ++i)
+                total_points += points.fastest_lap;
+        };
 
-    // Calculates how many points earned for finishing position in a Feature Race:
-    void feature_race_points(int position) {
-        if (position == 1)
-            total_points += points.first;
-        if (position == 2)
-            total_points += points.second;
-        if (position == 3)
-            total_points += points.third;
-        if (position == 4)
-            total_points += points.fourth;
-        if (position == 5)
-            total_points += points.fifth;
-        if (position == 6)
-            total_points += points.sixth;
-        if (position == 7)
-            total_points += points.seventh;
-        if (position == 8)
-            total_points += points.eighth;
-        if (position == 9)
-            total_points += points.ninth;
-        if (position == 10)
-            total_points += points.tenth;
-    }
+        // Adds points to driver's score for Feature Races.
+        // Calculates using number of times the specified finish position has been achieved:
+        //      fr_add_points_to_driver(4, 1, verstappen);
+        //      // Adds 4 x 1st place points to F1DriverPoints verstappen.
+        void fr_add_points_to_driver(int multiplier, int position, struct F1DriverPoints &driver) {
+            for (int i = 1; i <= multiplier; ++i)
+                driver.feature_race_points(position);
+        }
+
+        // Adds points to driver's score for Sprint Races. Same as fr_add_points_to_driver().
+        void sr_add_points_to_driver(int multiplier, int position, struct F1DriverPoints &driver) {
+            for (int i = 1; i <= multiplier; ++i)
+                driver.sprint_race_points(position);
+        }
+
+        // Calculates how many points earned for finishing position in a Sprint Race:
+        void sprint_race_points(int position) {
+            if (position == 1)
+                total_points += points.first_sprint;
+            if (position == 2)
+                total_points += points.second_sprint;
+            if (position == 3)
+                total_points += points.third_sprint;
+            if (position == 4)
+                total_points += points.fourth_sprint;
+            if (position == 5)
+                total_points += points.fifth_sprint;
+            if (position == 6)
+                total_points += points.sixth_sprint;
+            if (position == 7)
+                total_points += points.seventh_sprint;
+            if (position == 8)
+                total_points += points.eighth_sprint; 
+        }
+
+        // Calculates how many points earned for finishing position in a Feature Race:
+        void feature_race_points(int position) {
+            if (position == 1)
+                total_points += points.first;
+            if (position == 2)
+                total_points += points.second;
+            if (position == 3)
+                total_points += points.third;
+            if (position == 4)
+                total_points += points.fourth;
+            if (position == 5)
+                total_points += points.fifth;
+            if (position == 6)
+                total_points += points.sixth;
+            if (position == 7)
+                total_points += points.seventh;
+            if (position == 8)
+                total_points += points.eighth;
+            if (position == 9)
+                total_points += points.ninth;
+            if (position == 10)
+                total_points += points.tenth;
+        }
+
+        // Getter for total_points.
+        int get_total_points() {
+            return total_points;
+        }
 };
 
-// FEATURE RACE
-// Adds points to a driver's total according to position and how many times that position was achieved:
-void fr_add_points_to_driver(int multiplier, int position, struct F1DriverPoints &driver) {
-    for (int i = 1; i <= multiplier; ++i)
-        driver.feature_race_points(position);
-}
-
-// SPRINT RACE
-// Adds points to a driver's total according to position and how many times that position was achieved:
-void sr_add_points_to_driver(int multiplier, int position, struct F1DriverPoints &driver) {
-    for (int i = 1; i <= multiplier; ++i)
-        driver.sprint_race_points(position);
-}
-
 int main() {
+    // Create F1DriverPoints driver for Max Verstappen:
     F1DriverPoints verstappen;
-    verstappen.total_points = 0;  // Initialize points to 0.
     
     // FEATURE RACE POINTS TALLY
     // Add 1st place finish points:
-    fr_add_points_to_driver(8, 1, verstappen);
+    verstappen.fr_add_points_to_driver(8, 1, verstappen);
         
     // Add 2nd place finish points:
-    fr_add_points_to_driver(4, 2, verstappen);
+    verstappen.fr_add_points_to_driver(4, 2, verstappen);
     
     // Add 3rd place finish points:
-    fr_add_points_to_driver(1, 3, verstappen);
+    verstappen.fr_add_points_to_driver(1, 3, verstappen);
     
     // Add 4th place finish points:
-    fr_add_points_to_driver(1, 4, verstappen);
+    verstappen.fr_add_points_to_driver(1, 4, verstappen);
     
     // Add 5th place finish points:
-    fr_add_points_to_driver(4, 5, verstappen);
+    verstappen.fr_add_points_to_driver(4, 5, verstappen);
 
     // Add 6th place finish points:
-    fr_add_points_to_driver(3, 6, verstappen);
+    verstappen.fr_add_points_to_driver(3, 6, verstappen);
 
     // SPRINT RACE POINTS TALLY
     // Add 1st place finish points:
-    sr_add_points_to_driver(4, 1, verstappen);
+    verstappen.sr_add_points_to_driver(4, 1, verstappen);
     
     // Add 4th place finish points:
-    sr_add_points_to_driver(1, 4, verstappen);
+    verstappen.sr_add_points_to_driver(1, 4, verstappen);
 
     // Add points for number of fastest laps:
     verstappen.fastest_lap(3);
 
-    printf("Max Verstappen total points: %d\n", verstappen.total_points);
+    printf("Max Verstappen total points: %d\n", verstappen.get_total_points());
 }
 
 // Max Verstappen's points for 2024 season as of 27/Nov/2024 = 403
